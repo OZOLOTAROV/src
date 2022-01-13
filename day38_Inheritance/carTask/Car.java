9a1ae44669c88897a9081b59376505b4554e9f69
OZOLOTAROV <oleksandrzolotarov94@gmail.com>
2022-01-13 09:38:20 -0600

    Day38 : Inheritance Continue - Method Overriding

--- day38_Inheritance/carTask/Car.java
+++ day38_Inheritance/carTask/Car.java
@@ -2,14 +2,15 @@ package day38_Inheritance.carTask;
 
 public class Car {
 
-    public String brand, model, color;
+    public String brand, model;
     public int year;
     public double price;
-    public long miles;
+    public String color;
+    public int miles;
 
 
 
-    public Car(String brand, String model, int year, double price,String color, long miles) {
+    public Car(String brand, String model, int year, double price, String color, int miles) {
         this.brand = brand;
         this.model = model;
         this.year = year;
@@ -17,4 +18,53 @@ public class Car {
         this.color = color;
         this.miles = miles;
     }
+
+    public void start(){
+        System.out.println(brand + " " + model + " is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand + " " + model + " is driving");
+    }
+
+    /* // fly is not a common for all cars
+   public void fly(){
+      }
+ */
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
+/*
+  carTask:
+    Create a class called Car
+        instance variables:
+            brand, model, year, price, color, miles
+
+            add a constructor to set all the fields
+
+        instance methods:
+            start(), drive(),toString()
+
+    Create the following sub classes of Car:
+        1. Toyota:
+                extra methods:
+                    reliable()
+
+        2. BMW:
+                extra methods:
+                    breaksDown()
+                    racing()
+
+        3. Tesla:
+                extra methods:
+                    autoPilot();
+ */
