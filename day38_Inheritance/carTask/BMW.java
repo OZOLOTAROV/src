1c51c025b7cc1249dab19dea7d233a5aa82a7f9f
OZOLOTAROV <oleksandrzolotarov94@gmail.com>
2022-01-13 10:03:59 -0600

    Day38 : Inheritance Continue - Method Overriding

--- day38_Inheritance/carTask/BMW.java
+++ day38_Inheritance/carTask/BMW.java
@@ -1,4 +1,21 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car {
+    public BMW(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand + " " + model + " is breaking down");
+    }
+    public void racing(){
+        System.out.println( brand + " " + model + " is a racing car");
+    }
 }
+/*
+ 2. BMW:
+                extra methods:
+                    breaksDown()
+                    racing()
+
+ */
--- day38_Inheritance/carTask/Tesla.java
+++ day38_Inheritance/carTask/Tesla.java
@@ -1,4 +1,17 @@
 package day38_Inheritance.carTask;
 
-public class Tesla {
+public class Tesla extends Car {
+    public Tesla(String brand, String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autoPilotr(){
+        System.out.println( brand + " " + model + " in autopilot mode");
+    }
+
 }
+/*
+Tesla:
+                extra methods:
+                    autoPilot();
+ */
