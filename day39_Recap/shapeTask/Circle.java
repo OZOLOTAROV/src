5898bc4210ffd7f1278a91cc10b06e49f7c2f12f
OZOLOTAROV <oleksandrzolotarov94@gmail.com>
2022-01-14 13:13:40 -0600

    Day38 : Recap

--- day39_Recap/shapeTask/Circle.java
+++ day39_Recap/shapeTask/Circle.java
@@ -10,15 +10,49 @@ public class Circle extends Shape{
     }
 
     public void setRadius(double radius) {
-        if (radius <= 0){
-            System.err.println( "Invalid Radius: " + radius);
+        if(radius <= 0){
+            System.err.println("Invalid Radius: "+radius);
             System.exit(1);
         }
         this.radius = radius;
     }
 
-    public Circle(String name, double radius) {
-        super(name);
+    public Circle( double radius) {
+        super("Circle");
         setRadius(radius);
     }
+
+    @Override
+    public double area() {
+        return radius* radius* pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*radius*pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", pi='" + pi + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+
+
 }
+
+/*
+Circle extends Shape:
+		variables:
+			radius
+			pi (static)
+		Encapsulate the field
+		Add a constructor to set the filed
+		area(): radius * radius * pi
+		perimeter(): 2 * radius * pi
+		toString(): r, pi, area, perimeter
+ */
